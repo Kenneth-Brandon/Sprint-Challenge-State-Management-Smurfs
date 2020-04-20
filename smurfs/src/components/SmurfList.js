a6a63c144e51ee0f80fe3eb716c32f6d13cc078d
unknown <ken.brandon@aol.com>
2020-04-19 20:17:00 -0700

    added loading react-spinner

--- smurfs/src/components/SmurfList.js
+++ smurfs/src/components/SmurfList.js
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
+import Loader from 'react-loader-spinner';
 
 import { getSmurfs, deleteSmurf } from '../store/smurfActions';
 import SmurfItem from './SmurfItem';
@@ -11,15 +12,25 @@ function SmurfList(props) {
 
   return (
     <div className="smurf-list">
-      {props.smurfs.map((item) => {
-        return (
-          <SmurfItem
-            key={item.id}
-            smurf={item}
-            delete={() => props.deleteSmurf(item.id)}
-          />
-        );
-      })}
+      {props.isFetching && (
+        <Loader
+          type="Puff"
+          color="#00BFFF"
+          height={100}
+          width={100}
+          timeout={3000}
+        />
+      )}
+      {props.smurfs &&
+        props.smurfs.map((item) => {
+          return (
+            <SmurfItem
+              key={item.id}
+              smurf={item}
+              delete={() => props.deleteSmurf(item.id)}
+            />
+          );
+        })}
     </div>
   );
 }
--- smurfs/src/store/smurfActions.js
+++ smurfs/src/store/smurfActions.js
@@ -7,11 +7,11 @@ export function getSmurfs() {
     axios
       .get('http://localhost:3333/smurfs')
       .then((res) => {
+        dispatch({ type: act.FETCH_SMURFS_FINISH });
         dispatch({
           type: act.FETCH_SMURFS_RECEIVED,
           payload: res.data,
         });
-        dispatch({ type: act.FETCH_SMURFS_FINISH });
       })
       .catch((err) => {
         dispatch({ type: act.FETCH_SMURFS_FINISH });
@@ -20,16 +20,18 @@ export function getSmurfs() {
 }
 
 export function postSmurf(data) {
+  console.log('post action data: ', data);
+
   return (dispatch) => {
     dispatch({ type: act.FETCH_SMURFS_START });
     axios
       .post('http://localhost:3333/smurfs', data)
       .then((res) => {
+        dispatch({ type: act.FETCH_SMURFS_FINISH });
         dispatch({
           type: act.FETCH_SMURFS_RECEIVED,
           payload: res.data,
         });
-        dispatch({ type: act.FETCH_SMURFS_FINISH });
       })
       .catch((err) => {
         dispatch({ type: act.FETCH_SMURFS_FINISH });
@@ -45,12 +47,11 @@ export function deleteSmurf(id) {
     axios
       .delete(`http://localhost:3333/smurfs/${id}`)
       .then((res) => {
-        console.log('result:', res);
+        dispatch({ type: act.FETCH_SMURFS_FINISH });
         dispatch({
           type: act.FETCH_SMURFS_RECEIVED,
           payload: res.data,
         });
-        dispatch({ type: act.FETCH_SMURFS_FINISH });
       })
       .catch((err) => {
         console.log('error:', err);
